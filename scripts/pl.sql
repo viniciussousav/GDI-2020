7a8b2e861b6f274fa6d7bf7ff2c9227d4c1b6043
pvsaragao <pvsa@cin.ufpe.br>
2021-03-28 22:06:12 -0300

    adição de mais queries de PL, ainda faltam algumas

--- scripts/pl.sql
+++ scripts/pl.sql
@@ -33,19 +33,89 @@ EXCEPTION
 END;
 /
 
--- USA UMA PESSOA JA CADASTRADA
-CREATE OR REPLACE PROCEDURE insert_cliente (
-  c_id  IN CLIENTE.id%TYPE
+-- USA UMA PESSOA JA CADASTRADA PARA ADICIONAR COMO FUNCIONARIO
+CREATE OR REPLACE PROCEDURE insert_funcionario (
+  f_id              IN FUNCIONARIO.id%TYPE,
+  f_salario         IN FUNCIONARIO.salario%TYPE,
+  f_admissao        IN FUNCIONARIO.data_admissao%TYPE,
+  f_ctps            IN FUNCIONARIO.ctps%TYPE
 ) IS
 BEGIN
 INSERT INTO
-  Cliente (id)
+  Funcionario (id, salario, data_admissao, ctps)
 VALUES
-  (c_id);
+  (f_id, f_salario, f_admissao, f_ctps);
 COMMIT;
-END insert_cliente;
+END insert_funcionario;
 /
 
-EXECUTE insert_cliente (12345678910);
+EXECUTE insert_funcionario (50819283053, 10000, DATE '2020-01-01', 12345678);
+SELECT * FROM Funcionario WHERE id = 50819283053;
 
-SELECT * FROM Cliente WHERE id = 12345678910;
+CREATE OR REPLACE FUNCTION get_pedidos_ano (
+  ano NUMBER
+)
+RETURN NUMBER
+IS
+  total_pedidos NUMBER := 0;
+BEGIN
+  SELECT COUNT(id)
+  INTO total_pedidos
+  FROM Pedido
+  WHERE EXTRACT(YEAR FROM data_pedido) = ano;
+
+  RETURN total_pedidos;
+END;
+/
+
+DECLARE
+  pedidos NUMBER := 0;
+BEGIN
+  pedidos := get_pedidos_ano(2020);
+  IF pedidos >= 1000 THEN
+    DBMS_OUTPUT.PUT_LINE('Meta de vendas atingida! Número total de pedidos: ' || pedidos);
+  ELSIF pedidos > 0 AND pedidos < 1000 THEN
+    DBMS_OUTPUT.PUT_LINE('Meta de vendas não atingida! Número total de pedidos: ' || pedidos);
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('Nenhum pedido realizado no período.');
+  END IF;
+END;
+/
+
+CREATE OR REPLACE PROCEDURE lista_produtos
+IS
+  CURSOR prod_cur IS SELECT nome, estoque FROM Produto;
+  var_prod prod_cur%ROWTYPE;
+BEGIN
+  OPEN prod_cur;
+  LOOP
+    FETCH prod_cur INTO var_prod;
+    EXIT WHEN prod_cur%NOTFOUND;
+    DBMS_OUTPUT.PUT_LINE('Produto: ' || var_prod.nome || ' | Estoque: ' || var_prod.estoque);
+  END LOOP;
+  CLOSE prod_cur;
+END;
+/
+
+CALL lista_produtos;
+
+-- AJEITAR ISSO AQUI
+DECLARE
+  valor NUMBER;
+  desconto NUMBER;
+BEGIN
+  valor := 3200;
+  CASE
+  WHEN valor >= 5000 THEN
+    desconto := 0.15;
+  WHEN valor >= 3000 AND valor < 5000 THEN
+    desconto := 0.10;
+  WHEN valor >= 1500 AND valor < 3000 THEN
+    desconto := 0.05;
+  ELSE
+    desconto := 0.00;
+  END CASE;
+
+  DBMS_OUTPUT.PUT_LINE('Desconto de ' || desconto * 100 || '%');
+END;
+/
