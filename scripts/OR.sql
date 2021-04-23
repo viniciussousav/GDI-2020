c8116978bf478017115ff1c08e0ea94ee659692c
luan1221 <lssa@cin.ufpe.br>
2021-04-23 16:10:29 -0300

    member functions

--- scripts/OR.sql
+++ scripts/OR.sql
@@ -1,7 +1,7 @@
 -- DROP TYPE tp_Pessoa;
 -- --DROP TYPE tp_Codigo_postal;
--- DROP TYPE tp_Endereco_pessoa;
--- DROP TYPE tp_Telefone_pessoa;
+-- DROP TYPE tp_Endereco;
+-- DROP TYPE tp_Telefone;
 -- DROP TYPE tp_Cliente;
 -- DROP TYPE tp_Email_cliente;
 -- DROP TYPE tp_Endereco_entrega;
@@ -24,22 +24,14 @@
 
 -- CRIAÇÃO DOS TIPOS
 
-CREATE OR REPLACE TYPE tp_Telefone_pessoa AS OBJECT( --OK
-    id_pessoa VARCHAR2(11),
+CREATE OR REPLACE TYPE tp_Telefone AS OBJECT( --OK
 	telefone VARCHAR2(15)
 );
 /
 
-CREATE OR REPLACE TYPE varray_Telefone AS VARRAY (5) OF tp_Telefone_pessoa; --OK
+CREATE OR REPLACE TYPE varray_Telefone AS VARRAY (5) OF tp_Telefone; --OK
 /
 
-CREATE OR REPLACE TYPE tp_Endereco_pessoa AS OBJECT( --OK
-	id_pessoa VARCHAR2(11),
-	cep NUMBER,
-	numero NUMBER,
-    complemento VARCHAR2(50)
-);
-/
 --Remocao das restricoes nas criacoes de tipo
 --varray nao eh referenciavel
 CREATE OR REPLACE TYPE tp_Pessoa AS OBJECT ( --OK
@@ -47,36 +39,48 @@ CREATE OR REPLACE TYPE tp_Pessoa AS OBJECT ( --OK
     nome VARCHAR2(30), 
     sexo CHAR(1),
     data_nascimento DATE,
-    endereco REF tp_Endereco_pessoa,
     telefones varray_Telefone,
-    MEMBER PROCEDURE mostrar (SELF tp_Pessoa)
+    MEMBER PROCEDURE mostrar,
+		ORDER MEMBER FUNCTION ordenar_por_idade (p tp_Pessoa) RETURN NUMBER
 ) NOT FINAL NOT INSTANTIABLE;
 /
 
-CREATE OR REPLACE TYPE BODY tp_Pessoa AS --OK
-MEMBER PROCEDURE mostrar (SELF tp_Pessoa) IS
+CREATE OR REPLACE TYPE tp_Endereco AS OBJECT( --OK
+	cep NUMBER,
+	numero NUMBER,
+  complemento VARCHAR2(50)
+);
+/
 
-n INTEGER;
-i INTEGER;
+ALTER TYPE tp_Pessoa ADD ATTRIBUTE (
+  endereco REF tp_Endereco
+);
+/ 
+
+CREATE OR REPLACE TYPE BODY tp_Pessoa AS --OK
+MEMBER PROCEDURE mostrar IS
+DECLARE
+	n INTEGER;
+	i INTEGER;
 BEGIN
 	--INFORMAÇÕES
 	DBMS_OUTPUT.put_line(‘NOME: ’ || nome);
-    DBMS_OUTPUT.put_line(‘CPF: ’ || cpf);
-    DBMS_OUTPUT.put_line(‘SEXO: ’ || sexo);
-    DBMS_OUTPUT.put_line(‘DATA DE NASCIMENTO: ’ || TO_CHAR(data_nasci));
+  DBMS_OUTPUT.put_line(‘CPF: ’ || cpf);
+  DBMS_OUTPUT.put_line(‘SEXO: ’ || sexo);
+  DBMS_OUTPUT.put_line(‘DATA DE NASCIMENTO: ’ || TO_CHAR(data_nasci));
 
-    n:=telefones.COUNT;
-    DBMS_OUTPUT.put_line(‘TELEFONES: ’);
-        IF n > 0 THEN
-			FOR i in 1..n LOOP
-				DBMS_OUTPUT.put_line(i || telefones(i).telefone);
-			END LOOP;
-		ELSE
-			DBMS_OUTPUT.put_line(‘Não há telefones registrados.’);
-		END IF;
+  n:=telefones.COUNT;
+  DBMS_OUTPUT.put_line(‘TELEFONES: ’);
+  IF n > 0 THEN
+		FOR i in 1..n LOOP
+			DBMS_OUTPUT.put_line(i || telefones(i).telefone);
+		END LOOP;
+	ELSE
+		DBMS_OUTPUT.put_line(‘Não há telefones registrados.’);
+	END IF;
 
 	--ENDERECO
-    DBMS_OUTPUT.put_line(‘ID: ’ || Endereco_pessoa.id_pessoa);
+  DBMS_OUTPUT.put_line(‘ID: ’ || Endereco_pessoa.id_pessoa);
 	DBMS_OUTPUT.put_line(‘CEP: ’ || Endereco_pesso.cep);
 	DBMS_OUTPUT.put_line(‘NUMERO: ’ || Endereco_pesso.numero);
 	DBMS_OUTPUT.put_line(‘COMPLEMENTO: ’ || Endereco_pesso.complemento);
@@ -85,13 +89,22 @@ BEGIN
 	DBMS_OUTPUT.put_line(‘ID: ’ || Telefone_pessoa.id_pessoa);
 	DBMS_OUTPUT.put_line(‘TELEFONE: ’ || Telefone_pessoa.telefone);
 	END;
+
+	ORDER MEMBER FUNCTION ordenar_por_idade (p tp_Pessoa) RETURN NUMBER IS
+	BEGIN
+		IF SELF.data_nascimento <= p.data_nascimento THEN
+			return (1);
+		ELSE 
+			return (-1);
+		END IF;
+	END;
+
+
 END;
 /
 
 CREATE OR REPLACE TYPE tp_Pedido AS OBJECT( --OK
-    id NUMBER, 
-    id_cliente VARCHAR2(11),
-    cod_produto NUMBER,
+    id NUMBER,
     data_pedido DATE,
     valor_total NUMBER,
     meio_pagamento VARCHAR(50) --CHECK (meio_pagamento in ('Cartão de crédito', 'Boleto', 'Cartão de Débito')) isso se faz na criacao de tabelas
@@ -102,13 +115,16 @@ CREATE OR REPLACE TYPE nt_lista_Pedido AS TABLE OF tp_Pedido;
 /
 
 CREATE OR REPLACE TYPE tp_Cliente UNDER tp_Pessoa (
-    id VARCHAR2(11),
+  id VARCHAR2(11),
 	lista_pedidos nt_lista_Pedido
 );
 /
 
+ALTER TYPE tp_Pedido ADD ATTRIBUTE (
+	cliente REF tp_Cliente
+);
+
 CREATE OR REPLACE TYPE tp_Email_cliente AS OBJECT(
-    id_cliente VARCHAR2(11),
 	email VARCHAR2(50)
 );
 /
@@ -127,9 +143,31 @@ CREATE OR REPLACE TYPE tp_Produto AS OBJECT(
     preco_compra NUMBER,
     preco_venda NUMBER,
     estoque NUMBER,
-    fabricante VARCHAR2(50)
+    fabricante VARCHAR2(50),
+		MEMBER FUNCTION balanco (val NUMBER) RETURN NUMBER,
+		MAP MEMBER FUNCTION comparar_valor_estoque RETURN NUMBER
 );
 /
+
+ALTER TYPE tp_Pedido ADD ATTRIBUTE (
+  cod_produto REF tp_Produto
+);
+
+CREATE OR REPLACE TYPE BODY tp_Produto AS 
+   MEMBER FUNCTION balanco return NUMBER IS
+   DECLARE 
+      resultado NUMBER
+   BEGIN 
+	 		resultado := (SELF.preco_venda * SELF.estoque) - (SELF.preco_compra * SELF.estoque);
+      return resultado; 
+   END balanco;
+	
+	MAP MEMBER FUNCTION comparar_valor_estoque RETURN NUMBER IS
+	BEGIN
+		return SELF.preco_venda * SELF.estoque;
+	END;   
+END; 
+/ 
 
 CREATE OR REPLACE TYPE nt_Produto AS TABLE OF tp_Produto;
 /
@@ -137,8 +175,37 @@ CREATE OR REPLACE TYPE nt_Produto AS TABLE OF tp_Produto;
 CREATE OR REPLACE TYPE tp_Funcionario UNDER tp_Pessoa (
 	salario NUMBER,
 	data_admissao DATE,
-    ctps VARCHAR2(8)
+  ctps VARCHAR2(8)
 ) NOT FINAL;
+/
+
+CREATE OR REPLACE TYPE BODY tp_Funcionario AS --OK
+OVERRIDE MEMBER PROCEDURE mostrar IS
+
+BEGIN
+	--INFORMAÇÕES PESSOAIS
+	DBMS_OUTPUT.put_line(‘NOME: ’ || nome);
+  DBMS_OUTPUT.put_line(‘CPF: ’ || cpf);
+  DBMS_OUTPUT.put_line(‘SEXO: ’ || sexo);
+  DBMS_OUTPUT.put_line(‘DATA DE NASCIMENTO: ’ || TO_CHAR(data_nasci));
+
+
+	--ENDERECO
+  DBMS_OUTPUT.put_line(‘ID: ’ || Endereco_pessoa.id_pessoa);
+	DBMS_OUTPUT.put_line(‘CEP: ’ || Endereco_pesso.cep);
+	DBMS_OUTPUT.put_line(‘NUMERO: ’ || Endereco_pesso.numero);
+	DBMS_OUTPUT.put_line(‘COMPLEMENTO: ’ || Endereco_pesso.complemento);
+
+    --TELEFONE
+	DBMS_OUTPUT.put_line(‘ID: ’ || Telefone_pessoa.id_pessoa);
+	DBMS_OUTPUT.put_line(‘TELEFONE: ’ || Telefone_pessoa.telefone);
+
+	--INFORMACOES TRABALHISTAS
+	DBMS_OUTPUT.put_line(‘SALARIO: ’ || salario);
+	DBMS_OUTPUT.put_line(‘DATA DE ADMISSAO: ’ || data_admissao);
+	DBMS_OUTPUT.put_line(‘CTPS: ’ || ctps);
+	END;
+END;
 /
 
 CREATE OR REPLACE TYPE tp_Dependente UNDER tp_Pessoa (
@@ -214,7 +281,7 @@ CREATE OR REPLACE TYPE tp_Registra AS OBJECT (
 
 -- CRIAÇÃO DAS TABELAS
 
-CREATE TABLE tb_Enderecos_pessoa OF tp_Endereco_pessoa 
+CREATE TABLE tb_Enderecos_pessoa OF tp_Endereco 
 (id_pessoa PRIMARY KEY);
 /
 
@@ -241,7 +308,7 @@ CREATE TABLE tb_Produto OF tp_Produto
 (cnpj PRIMARY KEY) NESTED TABLE produtos STORE AS produto_st;
 /
 	       
-CREATE TABLE tb_endereco_pessoa OF tp_endereco_pessoa (
+CREATE TABLE tb_endereco_pessoa OF tp_endereco (
     id_endereco                 PRIMARY KEY
 );
 /
@@ -260,7 +327,7 @@ CREATE TABLE tb_funcionario OF tp_funcionario (
 --     'João Silva', 
 --     'M', 
 --     DATE '1983-01-20', 
---     tp_Endereco_pessoa(86505080066, 45821630, 7, 'APT 503'),
+--     tp_Endereco(86505080066, 45821630, 7, 'APT 503'),
 --     varray_Telefone(071994021699)
 -- );
 
@@ -269,7 +336,7 @@ CREATE TABLE tb_funcionario OF tp_funcionario (
 --     'José Souza', 
 --     'M', 
 --     DATE '1990-03-28',
---     tp_Endereco_pessoa(28788212033, 29141860, 3, 'APT 211'),
+--     tp_Endereco(28788212033, 29141860, 3, 'APT 211'),
 --     varray_Telefone(027986158007)
 -- );
 
@@ -278,7 +345,7 @@ CREATE TABLE tb_funcionario OF tp_funcionario (
 --     'Maria Farias', 
 --     'F', 
 --     DATE '1991-05-03',
---     tp_Endereco_pessoa(30378752081, 63504130, 30, 'APT 702'),
+--     tp_Endereco(30378752081, 63504130, 30, 'APT 702'),
 --     varray_Telefone(088981407529)
 -- );
 
@@ -287,7 +354,7 @@ CREATE TABLE tb_funcionario OF tp_funcionario (
 --     'Ana Leite', 
 --     'F', 
 --     DATE '1988-07-25',
---     tp_Endereco_pessoa(90459533070, 13900325, 22, 'APT 105'),
+--     tp_Endereco(90459533070, 13900325, 22, 'APT 105'),
 --     varray_Telefone(015991625781)
 -- );
 	       
@@ -295,16 +362,16 @@ CREATE SEQUENCE ENDERECO_ID_SEQ;
 CREATE SEQUENCE FUNCIONARIO_ID_SEQ;
 
 INSERT INTO tb_endereco_pessoa
-VALUES  (tp_endereco_pessoa (ENDERECO_ID_SEQ.NEXTVAL, 50740370 , '78', 'Casa'));
+VALUES  (tp_endereco (ENDERECO_ID_SEQ.NEXTVAL, 50740370 , '78', 'Casa'));
 			     
 INSERT INTO tb_endereco_pessoa
-VALUES  (tp_endereco_pessoa (ENDERECO_ID_SEQ.NEXTVAL, 78555456 , '555', 'Apartamento'));
+VALUES  (tp_endereco (ENDERECO_ID_SEQ.NEXTVAL, 78555456 , '555', 'Apartamento'));
 			     
 INSERT INTO tb_endereco_pessoa
-VALUES  (tp_endereco_pessoa (ENDERECO_ID_SEQ.NEXTVAL, 12355578 , '4564', 'Sitio'));
+VALUES  (tp_endereco (ENDERECO_ID_SEQ.NEXTVAL, 12355578 , '4564', 'Sitio'));
 			     
 INSERT INTO tb_endereco_pessoa
-VALUES  (tp_endereco_pessoa (ENDERECO_ID_SEQ.NEXTVAL, 56425244 , '85', 'Fazenda'));
+VALUES  (tp_endereco (ENDERECO_ID_SEQ.NEXTVAL, 56425244 , '85', 'Fazenda'));
 			     
 INSERT INTO tb_funcionario
 VALUES  (tp_funcionario (FUNCIONARIO_ID_SEQ.NEXTVAL, '111.222.333-88', 'Matheus', 'Masculino', TO_DATE ('11/11/1997', 'DD/MM/YYYY'),
